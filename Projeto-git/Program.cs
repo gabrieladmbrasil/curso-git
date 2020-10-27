542c496684afd2b3705024069ff3f5aebb14a2cd
Gabriel Lima <gabriel.admbrasil@gmail.com>
2020-10-27 20:14:49 -0300

    Inclusao de buenos dias

--- Projeto-git/Program.cs
+++ Projeto-git/Program.cs
@@ -7,6 +7,7 @@ namespace Projeto_git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("buenos dias!");
         }
     }
 }
